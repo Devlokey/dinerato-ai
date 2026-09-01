a9d94e33b9c748f64ab7f8c51ecf28095d9a0288
Devlokey <Devlokey@users.noreply.github.com>
2026-09-01 10:31:18 +0530

    fix: use HashRouter for 100% consistent routing and state parity between GitHub Pages and localhost

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App.jsx';
 import { ERPProvider } from './context/ERPContext.jsx';
 import { AgentProvider } from './context/AgentContext.jsx';
@@ -8,12 +8,12 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ERPProvider>
         <AgentProvider>
           <App />
         </AgentProvider>
       </ERPProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
